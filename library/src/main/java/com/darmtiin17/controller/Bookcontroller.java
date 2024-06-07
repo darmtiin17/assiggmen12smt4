bb778b8a820d4a3a71ca18ea297f348924dc7988
Muhammad Haidar Matin <114727612+darmtiin17@users.noreply.github.com>
2024-06-07 11:05:53 +0700

    Add files via upload

---
+++ library/src/main/java/com/darmtiin17/controller/Bookcontroller.java
@@ -0,0 +1,15 @@
+package com.darmtiin17.library_manager.controller;
+
+import com.darmtiin17.library_manager.model.Book;
+import com.darmtiin17.library_manager.service.BookService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+@RestController
+@RequestMapping("/api-books/")
+public class BookController {
+    @Autowired
+    private BookService     
+}
+}
---
+++ library/src/main/java/com/darmtiin17/library/LibraryApplication.java
@@ -0,0 +1,13 @@
+package com.darmtiin17.library_manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibraryApplication.class, args);
+	}
+
+}
